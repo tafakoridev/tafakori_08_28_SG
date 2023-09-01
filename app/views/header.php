81b1eda61a4315fc5c222d4a798a9b442f7f5cb0
tafakoriwork <tafakoriwork@gmail.com>
2023-09-01 19:03:18 +0330

    change css and js link in header and footer and add public directory to them

--- app/views/header.php
+++ app/views/header.php
@@ -4,5 +4,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SarvCRM Technical Assessment</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
 </head>
